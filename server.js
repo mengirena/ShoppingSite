963d2755e7efd3ac36b893c9bc955f7e17a28c76
Irena Chen <irena1009@gmail.com>
2021-07-07 17:57:15 -0700

    test article route

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 
+const ARTICLES = require("./constants/Articles")
+
 const PORT = process.env.PORT | 5000
 
 const app = express()
+
+app.get("/articles", (req, res, next) => {
+    res.json(ARTICLES)
+})
 
 app.listen(PORT, ()=>{
     console.log(`server is running at ${PORT}`)
