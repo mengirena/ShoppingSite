50087522622d97d9d1068bb8ae4e2863b8ddc08f
Irena Chen <irena1009@gmail.com>
2021-07-08 19:19:30 -0700

    test get method article

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const Articles = require("./articles/Articles");
 
+//list of article
 const ARTICLES = require("./constants/Articles")
 
 const PORT = process.env.PORT | 5000
 
 const app = express()
 
+const ResponseManagement = require('./common/handleResponses');
+
 app.get("/articles", (req, res, next) => {
     res.json(ARTICLES)
+})
+
+app.get("/articles/:articleId", (req, res, next) => {
+    const articleId = req.params.articleId
+    console.log(articleId)
+    console.log(Articles)
+    return  Articles.get(articleId)
+        .then(response => ResponseManagement.handleSuccessResponse(req, res, response))
+        .catch(error => ResponseManagment.handleErrorResponse(req, res, error))
 })
 
 app.listen(PORT, ()=>{
